6dbfe308c5be094cb2d5e6e9e3706d45b27648a0
Vishal Machchha <95867828+vishalIII@users.noreply.github.com>
2024-07-20 00:16:49 +0530

    Update axios.jsx

--- p3/Client/src/utils/axios.jsx
+++ p3/Client/src/utils/axios.jsx
@@ -2,7 +2,8 @@ import axe from 'axios';
 
 // Create an instance of axios with a base URL
 const axios = axe.create({
-  baseURL: 'http://localhost:3000', // Set your base URL here
+  // baseURL: 'http://localhost:3000', // Set your base URL here
+  baseURL: 'https://proappoint.onrender.com',
   timeout: 5000, // Optional timeout configuration
   headers: {
     'Content-Type': 'application/json',
